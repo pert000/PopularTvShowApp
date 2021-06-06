3287ab7622e730c7939638de7fd6ee861a2d7be8
User <pertiamariami@gmail.com>
2021-06-06 17:53:37 +0400

    details

--- app/src/main/java/com/example/populartvshowapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/populartvshowapp/ui/details/DetailsFragment.kt
@@ -1,16 +1,18 @@
 package com.example.populartvshowapp.ui.details
 
 
+import android.R
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.*
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.example.populartvshowapp.databinding.FragmentDetailsBinding
+import com.example.populartvshowapp.model.CreatedBy
 import com.example.populartvshowapp.ui.details.viewmodel.DetailsViewModel
 import com.example.spacexmp.utils.ExtraKeys
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,10 +28,22 @@ class DetailsFragment : Fragment() {
     ): View? {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
 
+
         arguments?.getInt(ExtraKeys.TV_SHOW_ID)?.let { viewModel.getDetails(it) };
         viewModel.detailsResponse.observe(viewLifecycleOwner, Observer {
+            Glide.with(this)
+                .load("https://image.tmdb.org/t/p/original//" + it.data?.poster_path)//todo
+                .into(binding.image)
+
+            binding.content.name.text = it.data?.name
+            binding.content.tagLine.text = it.data?.tagline
+            binding.content.overview.text = it.data?.overview
+            binding.content.homepage.text=it.data?.homepage
+            binding.content.creator.text = it.data?.created_by?.get(0)?.name //todo
 
         })
+
+
 
         return binding.root
     }
